049f0b84192240f320010f918e805f8a62c3783d
CPFlores00 <carlos_raul_98@outlook.com>
2020-04-17 20:01:11 -0700

    Update entry point

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ app.set('port', process.env.SERVER_PORT || 8080);
 
 // Middlewares
 app.use(morgan('dev'));
-app.use(express.json());
+app.use(express.json({ limit: '300kb' }));
 app.use(express.urlencoded({ extended: false }));
 
 // Routes
-//
+// app.use('api/v1', require('routes/'));
 
 // Run
 app.listen(app.get('port'), () => {
