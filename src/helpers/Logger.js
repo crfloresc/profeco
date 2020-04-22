eac97c303e8ea73df5446a13133cf839693b342b
CPFlores00 <carlos_raul_98@outlook.com>
2020-04-21 17:32:37 -0700

    Add some features

---
+++ src/helpers/Logger.js
@@ -0,0 +1,24 @@
+const chalk = require('chalk');
+
+class Logger {
+  info(msg) {
+    console.info(chalk.green(msg));
+  }
+  error(msg) {
+    console.error(chalk.red(msg));
+  }
+};
+
+const info = (...msg) => {
+  console.info(
+    chalk.green(msg)
+  );
+};
+
+const error = (...msg) => {
+};
+
+module.exports = {
+  info,
+  error
+};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const chalk = require('chalk');
+const { info, error } = require('./helpers/Logger');
 const morgan = require('morgan');
 const app = express();
 
@@ -8,17 +8,17 @@ app.set('port', process.env.SERVER_PORT || 8080);
 
 // Middlewares
 app.use(morgan('dev'));
-app.use(express.json({ limit: '300kb' }));
+app.use(express.json(/*{ limit: '300kb' }*/));
 app.use(express.urlencoded({ extended: false }));
 
 // Routes
-// app.use('api/v1', require('routes/'));
+app.use('api/v1', require('./routes/catalog.routes'));
 
 // Run
 app.listen(app.get('port'), () => {
-  console.info(chalk.green('[server] app listening on', app.get('port')));
+  info('[server] app listening on '+ app.get('port'));
 }).on('close', () => {
-  console.info(chalk.green('[server] app stopped'));
+  info('[server] app stopped');
 }).on('error', (err) => {
-  console.error(chalk.red('[server] app error'), err.message)
+  error('[server] app error ' + err.message)
 });
