05417e828cf261b4abc6feb3317c3491b00c7d2a
CPFlores00 <carlos_raul_98@outlook.com>
2020-04-28 20:23:58 -0700

    Fix connection with mongo and clean up

--- src/data/database.js
+++ src/data/database.js
@@ -1,33 +1,22 @@
 const mongoose = require('mongoose');
-const chalk = require('chalk');
+const { info, error } = require('../helpers/Logger');
 
 const optionsdb = {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
 };
 
 const uri = process.env.MONGOLAB_URI || 'mongodb://admin:pass1234@ds161890.mlab.com:61890/profeco';
 
 mongoose.connect(uri, optionsdb);
-// mongoose.set('debug', true);
 
 mongoose.connection.on('connected', () => {
-  console.info(chalk.green('[database] connected'));
+  info('[database] connected');
 }).on('error', (err) => {
-  console.error(chalk.red(`[database] error ${err.message}`));
+  error(`[database] error ${err.message}`);
   process.exit();
 });
-
-const connect = () => {
-  mongoose.connect(uri, optionsdb);
-};
-
-const disconnect = () => {
-  mongoose.disconnect();
-};
-
-const drop = () => {
-  mongoose.connection.dropDatabase();
-};
 
 module.exports = mongoose;
