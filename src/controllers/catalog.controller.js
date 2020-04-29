90a09cafc7bf5e983885d0a5b24fefa8c3b52f8d
CPFlores00 <carlos_raul_98@outlook.com>
2020-04-29 00:55:46 -0700

    Rename model of product to product.model

--- src/controllers/catalog.controller.js
+++ src/controllers/catalog.controller.js
@@ -1,4 +1,4 @@
-const Product = require('../models/Product');
+const Product = require('../models/product.model');
 const { info, error } = require('../helpers/Logger');
 
 // TODO: implement validation, auth req
