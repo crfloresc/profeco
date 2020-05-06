2ec229b356be705999036fa68fb2383a862c9b9e
CPFlores00 <carlos_raul_98@outlook.com>
2020-05-06 16:33:42 -0700

    Refactor on models

--- src/models/review.model.js
+++ src/models/review.model.js
@@ -2,10 +2,14 @@ const mongoose = require('../data/database');
 
 /**
  * Schema for review:
- * - comment: NA
- * - rating: NA
- * - idProduct: NA
- * - idUser: NA
+ * - comment: indica el comentario que se va a realizar
+ *    de tal producto.
+ * - rating: indica las estrellas del 1 al 5 que se van
+ *    a dejar al producto, siendo 5 como excelente.
+ * - idProduct: indica el id del producto relacionado
+ *    a tal comentario.
+ * - idUser: indica el id del usuario que realizo
+ *    la review relacionado a tal comentario.
  */
 const reviewSchema = new mongoose.Schema({
   comment: {
